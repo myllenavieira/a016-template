62733e4f001476e2ba2ec204cb4c07d7f31c096d
myllenavieira <vieirasilvamyllena@gmail.com>
2022-08-21 16:50:27 -0300

    Prática Guiada

--- script.js
+++ script.js
@@ -35,3 +35,72 @@ const filmes = [
 ]
 
 // escreva seu código abaixo 👇🏻
+
+// for (let i = 0; i < filmes.length; i++) {
+//   console.log(`${filmes[i].titulo}, de ${filmes[i].ano}, dirigido por ${filmes[i].diretor}.`);
+//   let elenco = `Tem elenco: `
+//   for (let j = 0; j < filmes[i].elenco.length; j++) {
+//     if (j+1 == filmes[i].elenco.length) {
+//       elenco += `${filmes[i].elenco[j]}.`
+//     } else {
+//       elenco += `${filmes[i].elenco[j]}, `
+//     }
+//   }
+//   console.log(elenco);
+// }
+
+
+// for (let i in filmes) {
+//   console.log(`${filmes[i].titulo}, de ${filmes[i].ano}, dirigido por ${filmes[i].diretor}`);
+//   let elenco = `Tem no elenco:`;
+//     for (let j of filmes[i].elenco) {
+//       elenco += ` ${j},`;
+//     }
+//     console.log(elenco);
+// }
+  
+
+
+
+
+
+
+// const megaSena = [
+//   [1, 2 ,3 , 4, 5, 6],
+//   [11, 22, 33, 44, 55, 66],
+//   [10, 20, 30, 40, 50, 60],
+//   [9, 19, 29, 39, 49, 59],
+// ]
+//exercício 1
+// if(megaSena.length === 4){
+
+//   for(let i =0; i<megaSena.length; i++){
+//     let sorteio = `Sorteio ${i+1}: `
+//     //lê os arrays completos e nomeia o número do sorteio
+//     for (let j = 0; j < megaSena[i].length; j++) {
+//       sorteio += `${megaSena[i][j]} `;
+//       //lê os números dentro do array que foi lido antes
+//     }
+//     console.log(sorteio);
+//   }
+// }else{
+//   console.log(`É necessário alterar o número de itens do array`);
+// }
+
+//exercício 2 e 3
+// if(megaSena.length === 4){
+
+//   for(let i in megaSena){
+//     //o i aqui é uma string
+//     let sorteio = `Sorteio ${Number(i)+1}: `
+//     //lê os arrays completos e nomeia o número do sorteio
+//     for(let j of megaSena[i]){
+//       //o j já vai trazer o valor 
+//       sorteio += `${[j]} `;
+//       //lê os números dentro do array que foi lido antes
+//     }
+//     console.log(sorteio);
+//   }
+// }else{
+//   console.log(`É necessário alterar o número de itens do array`);
+// }
